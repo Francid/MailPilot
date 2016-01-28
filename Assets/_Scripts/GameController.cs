4007a893923f6863886c4cd3422754aecba7534d
francid <francid81@gmail.com>
2016-01-27 19:54:04 -0500

    Added GameObject

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	//PUBLIC INSTANCE VARIABLE
+	public int cloudNumber = 3;
+	public CloudController cloud;
+
+	// Use this for initialization
+	void Start () {
+		this._Initialize ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// PRIVATES METHODS
+	private void _Initialize(){
+		for (int cloudCount = 0; cloudCount < this.cloudNumber; cloudCount++) {
+			Instantiate (this.cloud.gameObject);
+		}
+	}
+}
