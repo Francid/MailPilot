68cd4ad7de42a16ac3ae7d99c5d2b3a84c559331
francid <francid81@gmail.com>
2016-02-03 20:04:52 -0500

    Added a score board

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -1,11 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
+
+	//PRIVATE INSTANCE VARIABLE
+	private int _scoreValue;
+	private int _livesValue;
+
+	// PUBLIC ACCESS METHODS
+	public int ScoreValue{
+		get{
+			return this._scoreValue;
+		}
+		set{ 
+			this._scoreValue = value;
+			this.scoreLabel.text = "Score: " + this._scoreValue;
+		}
+	}
+
+	public int LivesValue{
+		get{
+			return this._livesValue;
+		}
+		set{ 
+			this._livesValue = value;
+			if (this._livesValue <= -1) {
+				this._endGame ();
+			}
+			this.livesLabel.text = "Lives: " + this._livesValue;
+		}
+	}
 
 	//PUBLIC INSTANCE VARIABLE
 	public int cloudNumber = 3;
 	public CloudController cloud;
+	public PlaneController plane;
+	public IslandController island;
+	public Text livesLabel;
+	public Text scoreLabel;
+	public Text gameOverLabel;
+	public Text highScoreLabel;
+
 
 	// Use this for initialization
 	void Start () {
@@ -19,8 +55,24 @@ public class GameController : MonoBehaviour {
 
 	// PRIVATES METHODS
 	private void _Initialize(){
+
+		this.ScoreValue = 0;
+		this.LivesValue = 3;
+		this.gameOverLabel.gameObject.SetActive (false);
+		this.highScoreLabel.gameObject.SetActive (false);
+
 		for (int cloudCount = 0; cloudCount < this.cloudNumber; cloudCount++) {
 			Instantiate (this.cloud.gameObject);
 		}
+	}
+
+	private void _endGame(){
+		this.highScoreLabel.text = "High Score: " + this._scoreValue;
+		this.gameOverLabel.gameObject.SetActive (true);
+		this.livesLabel.enabled = false;
+		this.scoreLabel.enabled = false;
+		this.highScoreLabel.gameObject.SetActive (true);
+		this.plane.gameObject.SetActive (false);
+		this.island.gameObject.SetActive (false);
 	}
 }
---
+++ Assets/_Scripts/PlaneCollider.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneCollider : MonoBehaviour {
+
+	//PUBLIC INSTANCE VARIABLE
+	public GameController gameController;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnTriggerEnter2D(Collider2D other){
+
+		if(other.gameObject.CompareTag("Island")){
+			// Play the sound
+			this.gameController.ScoreValue += 100;
+		}
+		if(other.gameObject.CompareTag("Cloud")){
+			//Play the Sound
+			this.gameController.LivesValue -= 1;
+		}
+
+	}
+}
